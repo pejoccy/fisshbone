3479aa02223f68e830869c31bc1406cd5a00a55e
Josh Emmy <pejoccy@yahoo.com>
2018-01-12 17:52:06 +0100

    updated

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+include './src/executeMaxJobs.php';
+include './src/printProfiles.php';
+
+$A = [7, 6, 5, 3, 4, 2, 1];
+
+
+$players[] = [
+ "Name" => "Ronaldo", 
+ "Age" => 31, 
+ "Country" => "Portugal",
+ "Team" => "Real Madrid",
+];
+$players[] = [
+ "Name" => "Messi", 
+ "Age" => 27, 
+ "Country" => "Argentina",
+ "Team" => "Barcelona"
+];
+$players[] = [
+ "Name" => "Neymar", 
+ "Age" => 24, 
+ "Country" => "Brazil",
+ "Team" => "Barcelona"
+];
+$players[] = [
+ "Name" => "Rooney", 
+ "Age" => 30, 
+ "Country" => "England",
+ "Team" => "Man United"
+];
+var_dump(formatPlayer($players)); exit;
--- src/executeMaxJobs.php
+++ src/executeMaxJobs.php
@@ -1,18 +1,17 @@
 <?php
 
-function executeMaxJobs(array $jobs, $time_allowed) {
+function executeMaxJobs(array $tasks, $time_allowed) {
     $jobs_done = 0;
-    if (empty($jobs)) { 
+    if (empty($tasks)) {
         return $jobs_done;
     }
-    $optimized_jobs = asort($jobs);
+    asort($tasks);   // optimize job 
     $remaining_time = $time_allowed;
-    foreach($optimized_jobs as $val) {
-        $remaining_time -= $val;
+    foreach ($tasks as $job_time) {
+        $remaining_time -= $job_time;
         if ($remaining_time < 0) {
             return $jobs_done;
         }
         $jobs_done++;
     }
 }
-
